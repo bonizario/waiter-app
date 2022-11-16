7c450013e90aa80753995b8b1c9322a36514436f
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2022-11-15 21:19:06 -0300

    fix(api): rename createCategory and createProduct

--- api/src/app/useCases/categories/createCategory.ts
+++ api/src/app/useCases/categories/createCategory.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 import { Category } from '../../models/Category';
 
-export async function createCategories(request: Request, response: Response) {
+export async function createCategory(request: Request, response: Response) {
   try {
     const { icon, name } = request.body;
     const category = await Category.create({ icon, name });
