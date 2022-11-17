4e41d2c6e4d59336750899c0a2e87fb7733b5fc5
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2022-11-17 13:05:28 -0300

    feat(mobile): setup local fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,23 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Main } from './src/Main';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <Main />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#222',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- mobile/src/components/Text.ts
+++ mobile/src/components/Text.ts
@@ -3,14 +3,14 @@ import styled from 'styled-components/native';
 interface TextProps {
   weight?: '400' | '600' | '700';
   color?: string;
-  size?: number;
-  opacity?: number;
+  size?: string;
+  opacity?: string;
 }
 
 export const Text = styled.Text<TextProps>`
   font-family: ${({ weight }) =>
     weight ? `GeneralSans-${weight}` : 'GeneralSans-400'};
-  color: ${({ color }) => color || '#333'};
+  color: ${({ color }) => color || '#333333'};
   font-size: ${({ size }) => (size ? `${size}px` : '16px')};
   opacity: ${({ opacity }) => opacity || 1};
 `;
