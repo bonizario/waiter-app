f9727488f659497b1bd68414250480dc760edd16
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2022-11-17 13:10:19 -0300

    feat(mobile): add SafeAreaView specific by OS

---
+++ mobile/src/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from '../components/Text';
+import { Container } from './styles';
+
+export function Main() {
+  return (
+    <Container>
+      <Text>Main Renderizou</Text>
+    </Container>
+  );
+}
---
+++ mobile/src/Main/styles.ts
@@ -0,0 +1,8 @@
+import { Platform, StatusBar } from 'react-native';
+import styled from 'styled-components/native';
+
+const isAndroid = Platform.OS === 'android';
+
+export const Container = styled.SafeAreaView`
+  margin-top: ${isAndroid ? `${StatusBar.currentHeight}px` : '0'};
+`;
