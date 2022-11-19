449b784de26ab7566513bb2141a5a4d8530492ff
Gabriel Bonizário <gabrielbonizario98@gmail.com>
2022-11-19 01:44:30 -0300

    feat(web): connect Orders component with api

--- web/src/components/Orders/index.tsx
+++ web/src/components/Orders/index.tsx
@@ -1,57 +1,24 @@
+import { useEffect, useState } from 'react';
+
 import { Order } from '../../@types/Order';
+import { api } from '../../utils/api';
 import { OrdersBoard } from '../OrdersBoard';
 import { Container } from './styles';
 
-const orders: Order[] = [
-  {
-    _id: '63742b74a71c78c0a411ac75',
-    table: '123',
-    status: 'WAITING',
-    products: [
-      {
-        product: {
-          name: 'Pizza quatro queijos',
-          imagePath: '1668556008750_quatro-queijos.png',
-          price: 40,
-        },
-        quantity: 2,
-        _id: '63742b74a71c78c0a411ac76',
-      },
-      {
-        product: {
-          name: 'Coca-Cola',
-          imagePath: '1668556805551_coca-cola.png',
-          price: 7,
-        },
-        quantity: 3,
-        _id: '63742b74a71c78c0a411ac77',
-      },
-    ],
-  },
-  {
-    _id: '63742bd9ea02db33eaa655bb',
-    table: '1',
-    status: 'WAITING',
-    products: [
-      {
-        product: {
-          name: 'Coca-Cola',
-          imagePath: '1668556805551_coca-cola.png',
-          price: 7,
-        },
-        quantity: 1,
-        _id: '63742bd9ea02db33eaa655bc',
-      },
-    ],
-  },
-];
-
 export function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    api.get('/orders').then(({ data }) => {
+      setOrders(data);
+    });
+  }, []);
+
   return (
     <Container>
       <OrdersBoard icon="🕒" title="Fila de espera" orders={orders} />
-      <OrdersBoard icon="👨‍🍳" title="Em preparação" orders={[]} />
-      <OrdersBoard icon="✅" title="Pronto!" orders={[]} />
+      <OrdersBoard icon="👨‍🍳" title="Em preparação" orders={orders} />
+      <OrdersBoard icon="✅" title="Pronto!" orders={orders} />
     </Container>
   );
 }
